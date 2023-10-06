0f13e6637d6de1b1d5c5c6c98d7404c22d7c843f
Olivier Ory <mail@normegil.be>
2023-10-06 21:23:35 +0200

    Tests

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,31 @@ fn main() {
 
     println!("Hello, {:?}", args);
 }
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+    use super::Args;
+
+    #[test]
+    fn arg_none() {
+        let args = Args::parse_from(&["cat-rs"]);
+        assert_eq!(args.input, None);
+    }
+
+    #[test]
+    fn arg_one() {
+        let args = Args::parse_from(&["cat-rs", "first_arg.txt"]);
+        assert_eq!(args.input, Some(vec!["first_arg.txt".to_string()]));
+    }
+
+    #[test]
+    fn arg_multiple() {
+        let args = Args::parse_from(&["cat-rs", "first_arg.txt", "second_arg.txt", "third_arg.txt"]);
+        assert_eq!(args.input, Some(vec![
+            "first_arg.txt".to_string(),
+            "second_arg.txt".to_string(),
+            "third_arg.txt".to_string()
+        ]));
+    }
+}
