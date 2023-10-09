a59b31e31281cbdee20ecadc039ad165b0914fd1
Olivier Ory <mail@normegil.be>
2023-10-09 15:36:46 +0200

    Test cases

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,24 @@ use thiserror::Error;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[arg(short, default_value_t = false)]
+    all: bool,
+
+    #[arg(short = 'e', default_value_t = false)]
+    show_end: bool,
+
+    #[arg(short = 't', default_value_t = false)]
+    show_tab: bool,
+
+    #[arg(short = 'E', default_value_t = false)]
+    show_end_partial: bool,
+
+    #[arg(short = 'T', default_value_t = false)]
+    show_tab_partial: bool,
+
+    #[arg(short = 'v', default_value_t = false)]
+    show_non_printing: bool,
+
     input: Option<Vec<String>>,
 }
 
@@ -18,15 +36,17 @@ enum Errors {
 fn main() {
     let args = Args::parse();
 
+    let output_opts = OutputOptions::from_args(&args);
+
     if args.input.is_none() {
-        if let Err(e) = read_from_stdin() {
+        if let Err(e) = read_from_stdin(output_opts) {
             panic!("{}", e);
         }
     } else if let Some(inputs) = args.input {
         for input in inputs {
             match fs::read_to_string(&input) {
                 Ok(content) => {
-                    println!("{}", content.trim());
+                    print_content(&output_opts, &content);
                 }
                 Err(e) => {
                     panic!("Error when reading {}: {}", &input, e);
@@ -36,13 +56,42 @@ fn main() {
     }
 }
 
-fn read_from_stdin() -> Result<(), Errors> {
+fn read_from_stdin(opts: OutputOptions) -> Result<(), Errors> {
     let stdin = io::stdin();
     loop {
         let mut buffer = String::new();
         stdin.read_line(&mut buffer)?;
-        println!("{}", buffer.trim());
+        print_content(&opts, &buffer)
     }
+}
+
+struct OutputOptions {
+    show_tabs: bool,
+    show_ends: bool,
+}
+
+impl OutputOptions {
+    fn from_args(args: &Args) -> Self {
+        let show_tabs =
+            args.all || args.show_tab || (args.show_non_printing && args.show_tab_partial);
+        let show_ends =
+            args.all || args.show_end || (args.show_non_printing && args.show_end_partial);
+        Self {
+            show_tabs,
+            show_ends,
+        }
+    }
+}
+
+fn print_content(opts: &OutputOptions, input: &str) {
+    let mut to_print = input.to_string();
+    if opts.show_tabs {
+        to_print = to_print.replace('\t', "^I");
+    }
+    if opts.show_ends {
+        to_print = to_print.replace('\n', "$\n");
+    }
+    println!("{}", to_print.trim())
 }
 
 #[cfg(test)]
@@ -72,8 +121,38 @@ mod tests {
             Some(vec![
                 "first_arg.txt".to_string(),
                 "second_arg.txt".to_string(),
-                "third_arg.txt".to_string()
+                "third_arg.txt".to_string(),
             ])
         );
+    }
+
+    #[test]
+    fn arg_show_tab() {
+        let args = Args::parse_from(&["cat-rs", "-t", "first_arg.txt"]);
+        assert!(args.show_tab);
+    }
+
+    #[test]
+    fn arg_show_tab_partial() {
+        let args = Args::parse_from(&["cat-rs", "-T", "first_arg.txt"]);
+        assert!(args.show_tab_partial);
+    }
+
+    #[test]
+    fn arg_show_end() {
+        let args = Args::parse_from(&["cat-rs", "-e", "first_arg.txt"]);
+        assert!(args.show_end);
+    }
+
+    #[test]
+    fn arg_show_end_partial() {
+        let args = Args::parse_from(&["cat-rs", "-E", "first_arg.txt"]);
+        assert!(args.show_end_partial);
+    }
+
+    #[test]
+    fn arg_show_non_printing() {
+        let args = Args::parse_from(&["cat-rs", "-v", "first_arg.txt"]);
+        assert!(args.show_non_printing);
     }
 }
